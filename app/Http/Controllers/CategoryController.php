bf234796164378fce8ce816d8463e8e7b21bfe1e
yedyaditya13 <yedyaditya@gmail.com>
2019-11-16 21:49:22 +0700

    adde edit.blade.php and create function edit, destroy, update on CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -32,4 +32,44 @@ class CategoryController extends Controller
             return redirect()->back()->with(['error' => $th->getMessage()]);
         }
     }
+
+    public function destroy($id) {
+        $categories = Category::findOrFail($id);
+        $categories->delete();
+
+        return redirect()->back()->with(['success' => 'Kategori: ' . $categories->name . 'Telah di hapus']);
+    }
+
+
+    public function edit($id){
+        $categories = Category::findOrFail($id);
+
+        return view('categories.edit', compact('categories'));
+    }
+
+
+    public function update(Request $request, $id){
+        // Validate form
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string'
+        ]);
+
+        try {
+            // Select data bedasarkan id
+            $categories = Category::findOrFail($id);
+
+            // Update data
+            $categories->update([
+                'name' => $request->name,
+                'description' =>$request->description
+            ]);
+
+             //redirect ke route kategori.index
+            return redirect(route('kategori.index'))->with(['success' => 'Kategori: ' .$categories->name . 'ditambahkan']);
+        } catch (\Throwable $th) {
+            //jika gagal, redirect ke form yang sama lalu membuat flash message error
+            return redirect()->back()->with(['error' => $th->getMessage()]);
+        }
+    }
 }
