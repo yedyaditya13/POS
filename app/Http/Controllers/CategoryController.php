2a421d20c0b4777fc1389e1e53ceb4e4afcf1e94
yedyaditya13 <yedyaditya@gmail.com>
2019-11-16 20:52:20 +0700

    finish layouts structure

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index() {
+        $categories = Category::orderBy('created_at', 'DESC')->paginate(10);
+        return view('categories.index', compact('categories'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::resource('/kategori', 'CategoryController')->except([
+    'create', 'show'
+]);
