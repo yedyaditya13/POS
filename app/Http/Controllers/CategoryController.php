5fc3ff0048b691bffc01711a5b08c582166af6d8
yedyaditya13 <yedyaditya@gmail.com>
2019-11-16 21:34:46 +0700

    create function store() on CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,4 +11,25 @@ class CategoryController extends Controller
         $categories = Category::orderBy('created_at', 'DESC')->paginate(10);
         return view('categories.index', compact('categories'));
     }
+
+
+    public function store(Request $request) {
+        // validate form
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string'
+        ]);
+
+        try {
+            $categories = Category::firstOrCreate([
+                'name' => $request->name],
+                [
+                'description' => $request->description
+            ]);
+            return redirect()->back()->with(['success' => 'Kategori : ' . $categories->name . 'ditambahkan']);
+
+        } catch (\Throwable $th) {
+            return redirect()->back()->with(['error' => $th->getMessage()]);
+        }
+    }
 }
