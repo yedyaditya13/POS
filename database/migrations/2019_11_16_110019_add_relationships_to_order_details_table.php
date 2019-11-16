2b96c6b432da8e1a4cd9f43684dc406fbc5ef76c
yedyaditya13 <yedyaditya@gmail.com>
2019-11-16 18:01:21 +0700

    finish add_relationships_to_orders__table and added add_relationships_to_order_details_table

--- database/migrations/2019_11_16_110019_add_relationships_to_order_details_table.php
+++ database/migrations/2019_11_16_110019_add_relationships_to_order_details_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddRelationshipsToOrdersTable extends Migration
+class AddRelationshipsToOrderDetailsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddRelationshipsToOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('order_details', function (Blueprint $table) {
             //
         });
     }
@@ -25,7 +25,7 @@ class AddRelationshipsToOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::table('orders', function (Blueprint $table) {
+        Schema::table('order_details', function (Blueprint $table) {
             //
         });
     }
