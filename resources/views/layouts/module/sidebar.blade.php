2b42520d95dad917d284d7e6279db45b4dbd9388
yedyaditya <yedyaditya@gmail.com>
2019-11-17 20:43:12 +0700

    creat view for users and edited sidebar for manajemen user and produk

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -52,6 +52,30 @@
                         </li>
                     </ul>
                 </li>
+
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fa fa-users"></i>
+                        <p>
+                            Manajemen Users
+                            <i class="right fa fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('role.index') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Role</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('users.index') }}" class="nav-link">
+                                <i class="fa fa-circle-o nav-icon"></i>
+                                <p>Users</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
 
@@ -68,24 +92,6 @@
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-        </li>
-
-        <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-                <i class="nav-icon fa fa-users"></i>
-                <p>
-                    Manajemen Users
-                    <i class="right fa fa-angle-left"></i>
-                </p>
-            </a>
-            <ul class="nav nav-treeview">
-                <li class="nav-item">
-                    <a href="{{ route('role.index') }}" class="nav-link">
-                        <i class="fa fa-circle-o nav-icon"></i>
-                        <p>Role</p>
-                    </a>
-                </li>
-            </ul>
         </li>
     </div>
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,11 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('/role', 'RoleController')->except([
         'create', 'show', 'edit', 'update'
     ]);
+
+    Route::resource('/users', 'UserController')->except([
+        'show'
+    ]);
+    Route::get('users/{id}', 'UserController@roles')->name('users.roles');
 });
 
 
