b2d04b57e3e5a758be36e59145bccef3e736a216
yedyaditya <yedyaditya@gmail.com>
2019-11-17 23:17:25 +0700

    set middleware permission or kernel helper and web routes

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,44 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::group(['middleware' => 'auth'], function () {
-    Route::resource('/kategori', 'CategoryController')->except([
-        'create', 'show'
-    ]);
 
-    Route::resource('/produk', 'ProductController');
+    // Route yang berada dalam group ini hanya dapat diakses oleh user
+    // yang memiliki role admin
+    Route::group(['middleware' => ['role:admin']], function () {
+        Route::resource('/role', 'RoleController')->except([
+            'create', 'show', 'edit', 'update'
+        ]);
 
+        Route::resource('/users', 'UserController')->except([
+            'show'
+        ]);
+        Route::get('/users/roles/{id}', 'UserController@roles')->name('users.roles');
+        Route::put('/users/roles/{id}', 'UserController@setRoles')->name('users.set_role');
+        Route::post('users/permission', 'UserController@addPermission')->name('users.add_permission');
+        Route::get('users/role-permission', 'UserController@rolePermission')->name('users.roles_permission');
+        Route::put('users/permission/{role}', 'UserController@setRolePermission')->name('users.setRolePermission');
+
+    });
+
+
+    //route yang berada dalam group ini, hanya bisa diakses oleh user
+    //yang memiliki permission yang telah disebutkan dibawah
+    Route::group(['middleware' => ['permission:show products|create products|delete products']], function () {
+        Route::resource('/kategori', 'CategoryController')->except([
+            'create', 'show'
+        ]);
+        Route::resource('/produk', 'ProductController');
+    });
+
+
+     //route group untuk kasir
+    Route::group(['middleware' => ['role:kasir']], function () {
+
+    });
+
+
+    //home  taruh diluar group karena semua jenis user yg login bisa mengaksesnya
     Route::get('/home', 'HomeController@index')->name('home');
-
-    // For role user permission
-    Route::resource('/role', 'RoleController')->except([
-        'create', 'show', 'edit', 'update'
-    ]);
-
-    Route::resource('/users', 'UserController')->except([
-        'show'
-    ]);
-    Route::get('users/{id}', 'UserController@roles')->name('users.roles');
-
-    Route::post('users/permission', 'UserController@addPermission')->name('users.add_permission');
-    Route::get('users/role-permission', 'UserController@rolePermission')->name('users.roles_permission');
-    Route::put('users/permission/{role}', 'UserController@setRolePermission')->name('users.setRolePermission');
 
 });
 
