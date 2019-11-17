0ad27e98d37256c1a52b1fedafbf23d28d72d322
yedyaditya <yedyaditya@gmail.com>
2019-11-17 19:45:07 +0700

    installation and konfig spatie package role permission

--- app/User.php
+++ app/User.php
@@ -6,6 +6,8 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     use Notifiable;
@@ -36,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // Using spatie package for role & permission
+    use Notifiable, HasRoles;
 }
